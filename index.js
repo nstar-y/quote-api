ca21cce69908a0697cb9a592bed4d931776ff538
Nastar <81209428+nstar-y@users.noreply.github.com>
2023-11-25 11:53:32 +0700

    hmm

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 require('dotenv').config({ path: './.env' })
 require('./app')
+const fetch = require('node-fetch')
+
+function keepAlive() {
+    const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
+if (/(\/\/|\.)undefined\./.test(url)) return
+    setInterval(() => {
+        fetch(url).catch(console.error)
+    }, 15 * 1000)
+    }
+
+keepAlive()
